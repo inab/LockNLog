60332cd5aeae52c65d90e28a1693b3a637fb0960
José María Fernández <jmfernandez@cnio.es>
2011-10-05 10:26:43 +0000

    *	Added control of the number of concurrent processes in mutex.
    git-svn-id: http://trac.bioinfo.cnio.es/svn/LockNLog/trunk@1489 e34b47a0-da62-479e-8d4e-c16c5edb7ed2

--- mutex_background.pl
+++ mutex_background.pl
@@ -7,32 +7,36 @@ use POSIX qw(setsid);
 use lib "$FindBin::Bin";
 use LockNLog::Mutex;
 
-# We have to ignore pleas from the children
-$SIG{CHLD}='IGNORE';
+if(scalar(@ARGV)>1) {
+	my $concurr = shift(@ARGV);
+	$concurr = ($concurr =~ /^[1-9][0-9]*$/)?$concurr:5;
+	# We have to ignore pleas from the children
+	$SIG{CHLD}='IGNORE';
 
-my($pid)=fork();
+	my($pid)=fork();
 
-if(defined($pid)) {
-	if($pid==0) {
-		# As we are using dumb forks, we must close connections to the parent process
-		close(STDIN);
-		open(STDIN,'<','/dev/null');
-		close(STDOUT);
-		open(STDOUT,'>','/dev/null');
-		close(STDERR);
-		open(STDERR,'>','/dev/null');
-		setsid();
-		
-		my($mutex)=LockNLog::Mutex->new(5);
+	if(defined($pid)) {
+		if($pid==0) {
+			# As we are using dumb forks, we must close connections to the parent process
+			close(STDIN);
+			open(STDIN,'<','/dev/null');
+			close(STDOUT);
+			open(STDOUT,'>','/dev/null');
+			close(STDERR);
+			open(STDERR,'>','/dev/null');
+			setsid();
+			
+			my($mutex)=LockNLog::Mutex->new($concurr);
 
-		$mutex->mutex(
-		sub {
-			system(@ARGV);
-		});
+			$mutex->mutex(
+			sub {
+				system(@ARGV);
+			});
+		} else {
+			# Nothing
+			exit 0;
+		}
 	} else {
-		# Nothing
-		exit 0;
+		die "ERROR: Unable to fork process!!!!";
 	}
-} else {
-	die "ERROR: Unable to fork process!!!!"
 }
